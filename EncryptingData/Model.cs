8852f4f2915c0b41a70a140c6a527ebf43aa8a6e
blaze6950 <d.progect21@gmail.com>
2018-06-20 13:43:29 +0300

    config encrypt and decipher for large files

--- EncryptingData/Model.cs
+++ EncryptingData/Model.cs
@@ -20,10 +20,12 @@ namespace EncryptingData
         private CancellationTokenSource _tokenSource;
         public event ProgressChanged OnProgressChanged;
         public event ActionEnded OnActionEnded;
+        private RijndaelManaged _RMCrypto;
 
         public Model()
         {
             _tokenSource = new CancellationTokenSource();
+            _RMCrypto = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
         }
 
         public string Path { get => _path; set => _path = value; }
@@ -36,38 +38,54 @@ namespace EncryptingData
 
         public void Encrypt(String password)
         {
+            FileStream fsCrypt = null;
+            CryptoStream cs = null;
+            FileStream fsIn = null;
             try
             {
                 UnicodeEncoding UE = new UnicodeEncoding();
                 byte[] key = UE.GetBytes(password);
 
                 string cryptFile = System.IO.Path.GetDirectoryName(_path) + "\\" + System.IO.Path.GetFileNameWithoutExtension(_path) + " enc" + System.IO.Path.GetExtension(_path);
-                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+                fsCrypt = new FileStream(cryptFile, FileMode.Create);
 
-                RijndaelManaged RMCrypto = new RijndaelManaged();
-
-                CryptoStream cs = new CryptoStream(fsCrypt,
-                    RMCrypto.CreateEncryptor(key, key),
+                _RMCrypto.Padding = PaddingMode.ANSIX923;
+                cs = new CryptoStream(fsCrypt,
+                    _RMCrypto.CreateEncryptor(key, key),
                     CryptoStreamMode.Write);
 
-                FileStream fsIn = new FileStream(_path, FileMode.Open);
+                fsIn = new FileStream(_path, FileMode.Open);
 
                 long fileLength = fsIn.Length;
                 long totalBytes = 0;
-                int data = 0;
-                while ((data = fsIn.ReadByte()) != -1)
+                double persentage = 0;                
+                int chunkSize;
+                checked
                 {
-                    totalBytes += data;
-                    double persentage = (double)totalBytes * 100.0 / fileLength;
-                    cs.WriteByte((byte)data);
-                    _tokenSource.Token.ThrowIfCancellationRequested();
-                    OnProgressChanged(persentage);
+                    chunkSize = (int)fsIn.Length / 100;
                 }
 
-
-                fsIn.Close();
-                cs.Close();
-                fsCrypt.Close();
+                for (long i = 0; i < fsIn.Length; i += chunkSize)
+                {                   
+                    byte[] chunkData = new byte[chunkSize];
+                    int bytesRead = 0;
+                    while ((bytesRead = fsIn.Read(chunkData, 0, chunkSize)) > 0)
+                    {
+                        totalBytes += bytesRead;
+                        persentage = (double)totalBytes * 100.0 / fileLength;                        
+                        if (bytesRead != chunkSize)
+                        {
+                            for (int x = bytesRead - 1; x < chunkSize; x++)
+                            {
+                                chunkData[x] = 0;
+                            }
+                        }
+                        cs.Write(chunkData, 0, bytesRead);
+                        OnProgressChanged(persentage);
+                        _tokenSource.Token.ThrowIfCancellationRequested();
+                    }
+                }
+                cs.FlushFinalBlock();
                 OnActionEnded();
             }
             catch (AggregateException ae)
@@ -75,48 +93,77 @@ namespace EncryptingData
                 if (ae.InnerException is OperationCanceledException)
                 {
                     MessageBox.Show("Encrypting was canceled!", "+");
+                    //DeleteFile(fsCrypt);
                 }
                 else
                 {
                     MessageBox.Show(ae.Message, "Oooops");
+                    //DeleteFile(fsCrypt);
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message, "Oooops");
+                //DeleteFile(fsCrypt);
+            }
+            finally
+            {
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();                
             }
         }
 
         public void StartDecipher(String password)
         {
+            FileStream fsCrypt = null;
+            CryptoStream cs = null;
+            FileStream fsOut = null;
             try
             {
                 UnicodeEncoding UE = new UnicodeEncoding();
                 byte[] key = UE.GetBytes(password);
                 string cryptFile = _path;
-                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Open);
+                fsCrypt = new FileStream(cryptFile, FileMode.Open);                
 
-                RijndaelManaged RMCrypto = new RijndaelManaged();
+                fsOut = new FileStream(cryptFile.Remove(cryptFile.IndexOf(" enc"), 4), FileMode.Create);
 
-                CryptoStream cs = new CryptoStream(fsCrypt,
-                    RMCrypto.CreateDecryptor(key, key),
-                    CryptoStreamMode.Read);
-
-                FileStream fsOut = new FileStream(cryptFile.Remove(cryptFile.IndexOf(" enc"), 4), FileMode.Create);
+                _RMCrypto.Padding = PaddingMode.None;
+                cs = new CryptoStream(fsOut,
+                    _RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Write);
                 long fileLength = fsCrypt.Length;
                 long totalBytes = 0;
-                int data = 0;
-                while ((data = cs.ReadByte()) != -1)
-                {
-                    fsOut.WriteByte((byte)data);
-                    double persentage = (double)totalBytes * 100.0 / fileLength;
-                    OnProgressChanged(persentage);
-                }
-                    
+                double persentage = 0;
 
-                fsOut.Close();
-                cs.Close();
-                fsCrypt.Close();
+
+                int chunkSize;
+                checked
+                {
+                    chunkSize = (int)fsCrypt.Length / 100;
+                }
+
+                for (long i = 0; i < fsCrypt.Length; i += chunkSize)
+                {
+                    byte[] chunkData = new byte[chunkSize];
+                    int bytesRead = 0;
+                    while ((bytesRead = fsCrypt.Read(chunkData, 0, chunkSize)) > 0)
+                    {
+                        totalBytes += bytesRead;
+                        persentage = (double)totalBytes * 100.0 / fileLength;                        
+                        if (bytesRead != chunkSize)
+                        {
+                            for (int x = bytesRead - 1; x < chunkSize; x++)
+                            {
+                                chunkData[x] = 0;
+                            }
+                        }
+                        cs.Write(chunkData, 0, bytesRead);
+                        OnProgressChanged(persentage);
+                        _tokenSource.Token.ThrowIfCancellationRequested();
+                    }
+                }
+                cs.FlushFinalBlock();
                 OnActionEnded();
             }
             catch (AggregateException ae)
@@ -124,16 +171,30 @@ namespace EncryptingData
                 if (ae.InnerException is OperationCanceledException)
                 {
                     MessageBox.Show("Decrypting was canceled!", "+");
+                    //DeleteFile(fsOut);
                 }
                 else
                 {
                     MessageBox.Show(ae.Message, "Oooops");
+                    //DeleteFile(fsOut);
                 }
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message, "Oooops");
+                //DeleteFile(fsOut);
             }
+            finally
+            {
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();                
+            }
+        }
+
+        private void DeleteFile(FileStream file)
+        {
+            File.Delete(file.Name);
         }
     }
 }
